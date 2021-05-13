42a48f329a228a8148039cbbc6c89fefbba6db7a
opavon <oriol.pavon.16@ucl.ac.uk>
2021-05-13 15:51:21 +0100

    Fix importFile and add openTDMSfile function

--- loose_seal/utilities.py
+++ loose_seal/utilities.py
@@ -4,22 +4,26 @@ import numpy as np
 import tkinter
 from tkinter.filedialog import askopenfilename, askopenfilenames
 from collections import defaultdict
+from nptdms import TdmsFile
 
 def importFile():
-    "What is this function doing?"
+    "__importFile__ will open a window where you can navigate and select a file to import. It will then store its path and call the __openFile__ function to exctract its contents."
+    
+    root = tkinter.Tk()
+    root.attributes("-topmost", True) # make window appear on top
+    in_path = askopenfilename() # open dialogue to select file
+    root.destroy() # close the root window
 
-    tkinter.Tk().withdraw() # Close the root window
-    in_path = askopenfilename()
-    folder_name = os.path.split(in_path)[0]
-    file_name = os.path.split(in_path)[1]
+    folder_name = os.path.split(in_path)[0] # get path until folder
+    file_name = os.path.split(in_path)[1] # get filename
     
     voltage_mV, current_pA, command, ttl, time, dt = openFile(in_path)
-
+    
     return voltage_mV, current_pA, command, ttl, time, dt, folder_name
 
 
 def openFile(in_path, curated_channel = None):
-    "What is this function doing?"
+    "__openFile__ will check whether you are attempting to open a tdms or a hdf5 file and will attempt to extract the data from each of the useful channels."
 
     if '.tdms' in in_path:
         extracted_channels, time, dt = openTDMSfile(in_path)
@@ -32,3 +36,26 @@ def openFile(in_path, curated_channel = None):
     ttl = extracted_channels[3]
     
     return voltage_mV, current_pA, command, ttl, time, dt
+
+def openTDMSfile(in_path, channel_list = ['Channel A', 'Channel B', 'Output A', 'Output B']):
+    "__openTDMSfile__ returns a list of arrays, where each is a sweep/trial i.e. [np.array(trial)]."
+    
+    tdms_file = TdmsFile(in_path)
+    
+    dataDict = defaultdict(list)
+    
+    for group in tdms_file.groups():
+        i=0
+        for sweep in group.channels():
+            dataDict[group.name].append(sweep.data)
+            i+=1
+                       
+    extracted_channels = []
+    
+    for channel in channel_list: # keep only useful channels
+        extracted_channels.append(dataDict[channel])
+    
+    time = dataDict['Time'][0]
+    dt = np.mean(np.diff(time))
+    
+    return extracted_channels, time, dt
