4981f48ec4d24c9a2814123589fc3b9a8956eddb
opavon <oriol.pavon.16@ucl.ac.uk>
2021-05-14 17:19:05 +0100

    Add docstrings and comments

--- loose_seal/utilities.py
+++ loose_seal/utilities.py
@@ -7,23 +7,29 @@ from collections import defaultdict
 from nptdms import TdmsFile
 
 def importFile():
-    "__importFile__ will open a window where you can navigate and select a file to import. It will then store its path and call the __openFile__ function to exctract its contents."
-    
-    root = tkinter.Tk()
-    root.attributes("-topmost", True) # make window appear on top
-    in_path = askopenfilename() # open dialogue to select file
-    root.destroy() # close the root window
+    """
+    `importFile` opens a window to select a file to import.
+    Returns path and calls `openFile` to extract data."
+    """
 
-    folder_name = os.path.split(in_path)[0] # get path until folder
-    file_name = os.path.split(in_path)[1] # get filename
+    root = tkinter.Tk()
+    root.attributes("-topmost", True) # Make window appear on top
+    in_path = askopenfilename() # Open dialogue to select file
+    root.destroy() # Close the root window
+
+    folder_name = os.path.split(in_path)[0] # Get path until folder
+    file_name = os.path.split(in_path)[1] # Get filename
     
-    voltage_mV, current_pA, command, ttl, time, dt = openFile(in_path)
+    voltage_mV, current_pA, command, ttl, time, dt = openFile(in_path) # Call openFile() function
     
-    return voltage_mV, current_pA, command, ttl, time, dt, folder_name
+    return voltage_mV, current_pA, command, ttl, time, dt, folder_name, file_name
 
 
 def openFile(in_path, curated_channel = None):
-    "__openFile__ will check whether you are attempting to open a tdms or a hdf5 file and will attempt to extract the data from each of the useful channels."
+    """
+    `openFile` checks whether you are attempting to open a `.tdms` or a `.hdf5` file.
+    Extracts the data from selected channels.
+    """
 
     if '.tdms' in in_path:
         extracted_channels, time, dt = openTDMSfile(in_path)
@@ -38,23 +44,28 @@ def openFile(in_path, curated_channel = None):
     return voltage_mV, current_pA, command, ttl, time, dt
 
 def openTDMSfile(in_path, channel_list = ['Channel A', 'Channel B', 'Output A', 'Output B']):
-    "__openTDMSfile__ returns a list of arrays, where each is a sweep/trial i.e. [np.array(trial)]."
+    """
+    `openTDMSfile` returns a list of arrays, where each is a sweep/trial.
+    """
     
-    tdms_file = TdmsFile(in_path)
-    
+    # Load .tdms file
+    tdms_file = TdmsFile(in_path) 
     dataDict = defaultdict(list)
     
+    # Iterate through channels and extract data from sweeps/trials
     for group in tdms_file.groups():
         i=0
         for sweep in group.channels():
             dataDict[group.name].append(sweep.data)
             i+=1
                        
+    # Keep only useful channels
     extracted_channels = []
-    
-    for channel in channel_list: # keep only useful channels
+
+    for channel in channel_list: 
         extracted_channels.append(dataDict[channel])
     
+    # Get time and delta_t
     time = dataDict['Time'][0]
     dt = np.mean(np.diff(time))
     
