f236c016ed35c4640963d91fc4ecd72f199b715a
opavon <oriol.pavon.16@ucl.ac.uk>
2021-09-29 10:54:25 +0100

    add save paths

--- loose_seal/loose_seal_analysis_pipeline.py
+++ loose_seal/loose_seal_analysis_pipeline.py
@@ -20,6 +20,7 @@ from IPython import get_ipython
 from utilities import * # includes functions importFile, openFile, openHDF5file, getLooseRseal, concatenateSweeps, findSpikes, cutSpikes, plotSpikesQC, spikesQC, cleanSpikes, averageSpikes, getSpikeParameters, getFiringRate, getInterspikeInterval, saveLooseSealResults
 print("done!")
 
+# %%
 # Load data for LIAM cell (contains spikes in test_pulse)
 channels_df, time, dt, folder_name, file_name = importFile(curated_channel = 'Sweeps_Analysis')
 print("file imported")
@@ -49,7 +50,7 @@ plotSpikesQC(file_name, peaks_properties, cut_spikes_baselined)
 
 # %%
 # Choose the QC metrics and remove detected peaks that correspond to noise and not spikes
-peaks_QC, cut_spikes_QC, cut_spikes_holding_QC, cut_spikes_baselined_QC, parameters_QC = spikesQC(file_name, peaks, peaks_properties, cut_spikes, cut_spikes_holding, cut_spikes_baselined, filter_by=['ph'], QC_ph_min=10)
+peaks_QC, cut_spikes_QC, cut_spikes_holding_QC, cut_spikes_baselined_QC, parameters_QC = spikesQC(file_name, peaks, peaks_properties, cut_spikes, cut_spikes_holding, cut_spikes_baselined, filter_by=['wh', 'pw', 'ph'], QC_wh_min=float('-inf'), QC_wh_max=float('inf'), QC_pw_min=float('-inf'), QC_pw_max=float('inf'), QC_ph_min=50, QC_ph_max=float('inf'))
 parameters_QC
 
 # %%
@@ -78,6 +79,9 @@ interspike_interval_df
 
 # %%
 # Save Results
-save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\loose_seal\analysis_sample_cells\results_test"
+vgat_ctrl_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\loose_seal\loose_seal_results\vgat_control"
+vgat_kynac_ptx_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\loose_seal\loose_seal_results\vgat_kynerinc_picrotoxin"
+vglut2_ctrl_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\loose_seal\loose_seal_results\vglut2_control"
+vglut2_ptx_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\loose_seal\loose_seal_results\vglut2_picrotoxin"
 
-saveLooseSealResults(save_path, file_name, sweep_IB_concatenated, pseudo_sweep_concatenated, peaks, cut_spikes, cut_spikes_holding, cut_spikes_baselined, peaks_QC, cut_spikes_QC, cut_spikes_holding_QC, cut_spikes_baselined_QC, cut_spikes_baselined_clean, average_spike, Rseal_df, peaks_properties, parameters_find_peaks, parameters_QC, parameters_clean, parameters_avg_spike, firing_frequency_df, spikes_by_sweep_df, spikes_by_window_df, interspike_interval_df)
+saveLooseSealResults(vgat_ctrl_save_path, file_name, sweep_IB_concatenated, pseudo_sweep_concatenated, peaks, cut_spikes, cut_spikes_holding, cut_spikes_baselined, peaks_QC, cut_spikes_QC, cut_spikes_holding_QC, cut_spikes_baselined_QC, cut_spikes_baselined_clean, average_spike, Rseal_df, peaks_properties, parameters_find_peaks, parameters_QC, parameters_clean, parameters_avg_spike, firing_frequency_df, spikes_by_sweep_df, spikes_by_window_df, interspike_interval_df)
