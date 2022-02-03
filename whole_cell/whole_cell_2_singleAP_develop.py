20aada6cbf41eedbb93531d41aae76d81a18ab74
opavon <oriol.pavon.16@ucl.ac.uk>
2022-02-03 17:46:33 +0000

    fix bugs after testing function

--- whole_cell/whole_cell_2_singleAP_develop.py
+++ whole_cell/whole_cell_2_singleAP_develop.py
@@ -348,11 +348,8 @@ def getSpikeParameters(
             # Get peak, trough, adp, ahp indices
             temp_avg_spike_peak_index = np.where(temp_avg_spike == max(temp_avg_spike))[0][0] # overall positive peak
             temp_avg_spike_trough_index = np.where(temp_avg_spike == min(temp_avg_spike[temp_avg_spike_peak_index:temp_avg_spike_peak_index+50]))[0][0] # negative peak within 2 ms after the action potential peak
-            temp_avg_spike_adp_index = np.where(temp_avg_spike == max(temp_avg_spike[temp_avg_spike_trough_index:temp_avg_spike_trough_index+200]))[0][0] # positive peak within 8 ms after the trough (afterdepolarization)
-            temp_avg_spike_ahp_index = np.where(temp_avg_spike == min(temp_avg_spike[temp_avg_spike_adp_index:temp_avg_spike_adp_index+1250]))[0][0] # negative peak within 50 ms after the afterdepolarization (afterhyperpolarization)
-            print(temp_avg_spike_trough_index)
-            print(temp_avg_spike_adp_index)
-            print(temp_avg_spike_ahp_index)
+            temp_avg_spike_adp_index = np.where(temp_avg_spike == max(temp_avg_spike[temp_avg_spike_trough_index+5:temp_avg_spike_trough_index+200]))[0][0] # positive peak within 8 ms after the trough (afterdepolarization)
+            temp_avg_spike_ahp_index = np.where(temp_avg_spike == min(temp_avg_spike[temp_avg_spike_trough_index+205:temp_avg_spike_trough_index+1250]))[0][0] # negative peak within 50 ms after the afterdepolarization (afterhyperpolarization)
 
             # Get peak, trough, adp, ahp metrics
             peak_mV = temp_avg_spike[temp_avg_spike_peak_index] # mV
@@ -369,21 +366,21 @@ def getSpikeParameters(
             adp_to_ahp_ms = (temp_avg_spike_ahp_index - temp_avg_spike_adp_index) * dt # ms
 
             # Get half_peak value, calculated by subtracting half the absolute magnitude (peak to trough) from the peak value
-            half_peak = peak_mV - (abs(peak_to_trough_magnitude) / 2) # mV
+            half_peak_mV = peak_mV - (abs(peak_to_trough_magnitude) / 2) # mV
             # Now, we need to get the first time we cross the half_peak value (before the peak), and the second time (after the peak). To do this, we are going to split the spike in two halves, and do an interpolation separately.
             from scipy.interpolate import interp1d # load function to interpolate
             temp_avg_spike_first_half = temp_avg_spike[:temp_avg_spike_peak_index] # get first half of average spike
             temp_1_f1 = interp1d(temp_avg_spike_first_half,
                                 range(0, len(temp_avg_spike_first_half)), 
                                 kind = "linear") # make function to interpolate
-            temp_half_width_start = temp_1_f1(half_peak) # find value corresponding to the half_peak
+            temp_half_width_start = temp_1_f1(half_peak_mV) # find value corresponding to the half_peak
 
             temp_avg_spike_second_half = temp_avg_spike[temp_avg_spike_peak_index:] # get second half of average spike
             temp_1_f2 = interp1d(temp_avg_spike_second_half, 
                                 range(temp_avg_spike_peak_index, 
                                 temp_avg_spike_peak_index+len(temp_avg_spike_second_half)), 
                                 kind = "linear") # make function to interpolate
-            temp_half_width_end = temp_1_f2(half_peak) # find value corresponding to the half_peak
+            temp_half_width_end = temp_1_f2(half_peak_mV) # find value corresponding to the half_peak
 
             # Subtract beginning from end and multiply by the sampling rate to obtain the half_width in ms
             half_width_ms = (temp_half_width_end - temp_half_width_start) * dt # ms
@@ -397,26 +394,11 @@ def getSpikeParameters(
             dvdt = diff_v / diff_t
             # Define action potential threshold as 5% of the maximum value of dV/dt
             dvdt_threshold_value = max(dvdt) * 0.05
-            print(dvdt_threshold_value)
-
-            from scipy.interpolate import interp1d # load function to interpolate
-            temp_2_f1 = interp1d(dvdt[:len(temp_avg_spike_first_half)],
-                                range(0, len(temp_avg_spike_first_half)), 
-                                kind = "linear") # make function to interpolate
-            dvdt_threshold_index = temp_2_f1(dvdt_threshold_value) # find index corresponding to threshold
-            print(dvdt_threshold_index)
-            print(np.where(dvdt > 20)[0][0])
-            # Once we have the index at which the dV/dt crosses our threshold, we need to find the value corresponding to the averaged spike. To do that, we will interplate the first half of the spike, but reversing the order of the variables. This will allow us to obtain the voltage at the index we just obtained. We will need to add 1 to the index to make up for the derivative.
-            temp_2_f2 = interp1d(range(0, len(temp_avg_spike_first_half)), 
-                                temp_avg_spike_first_half,
-                                kind = "linear") # make function to interpolate
-            spike_threshold_mV = temp_2_f2(dvdt_threshold_index + 1) # find value corresponding to threshold, adding one to make up for the index lost due to the derivative
-            # Now we have the calculated threshold for our average action potential. We can obtain the index in the average spike so we can compute further parameters.
-            temp_2_f3 = interp1d(temp_avg_spike_first_half,
-                                range(0, len(temp_avg_spike_first_half)), 
-                                kind = "linear") # make function to interpolate
-            spike_threshold_index = temp_2_f3(spike_threshold_mV) # find index corresponding to threshold
-
+            # Find the index in the dV/dt trace where this threshold is crossed
+            dvdt_threshold_index = np.where(dvdt > dvdt_threshold_value)[0][0]
+            # Find the voltage value in the average trace corresponding to the threshold index
+            spike_threshold_index = dvdt_threshold_index + 1 # add one to make up for the index lost due to the derivative
+            spike_threshold_mV = temp_avg_spike[spike_threshold_index]
             # Get threshold to peak
             threshold_to_peak_ms = (temp_avg_spike_peak_index - spike_threshold_index) * dt # ms
 
@@ -430,65 +412,61 @@ def getSpikeParameters(
                 """
             )
             axs['A'].plot(temp_avg_spike, 'k') # plot average spike trace
-            axs['A'].plot(int(spike_threshold_index), temp_avg_spike[int(spike_threshold_index)], "or")
+            axs['A'].plot(spike_threshold_index, temp_avg_spike[spike_threshold_index], "or")
             axs['A'].plot(temp_avg_spike_peak_index, temp_avg_spike[temp_avg_spike_peak_index], "or")
             axs['A'].plot(temp_avg_spike_trough_index, temp_avg_spike[temp_avg_spike_trough_index], "or")
             axs['A'].plot(temp_avg_spike_adp_index, temp_avg_spike[temp_avg_spike_adp_index], "oc")
             axs['A'].plot(temp_avg_spike_ahp_index, temp_avg_spike[temp_avg_spike_ahp_index], "oy")
-            axs['A'].set_title('Average spike', fontsize = 14)
+            axs['A'].set_title(f'Average spike \n {temp_file_id[0]}', fontsize = 14)
             axs['A'].set_ylabel('voltage [mV]', fontsize = 12)
             axs['A'].set_xlim([temp_avg_spike_peak_index-100, temp_avg_spike_peak_index+2000])
             axs['B'].plot(temp_avg_spike[1:], dvdt, 'k')
             axs['B'].set_title('Phase plot', fontsize = 14)
             axs['B'].set_xlabel('voltage [mV]', fontsize = 12)
             axs['B'].set_ylabel('dV/dt [mV/ms]', fontsize = 12)
-            axs['B'].set_xlim([-100, 100])
+            axs['B'].set_ylim([-700, 700])
             axs['B'].set_xlim([-100, 100])
             fig.canvas.manager.window.move(0, 0) # Move figure to top left corner
             plt.show(block = True) # Lets you interact with plot and proceeds once figure is closed
-
-            # Check whether spike analysis is complete
-            happy_results = input("Are you happy with the results for this recording? y/n")
-
-            if happy_results == 'y':
-                # Create dataframe of results across sweeps:
-                single_AP_parameters_df = pd.DataFrame([
-                    test_pulse_command_pA, injected_current_pA, holding_mV, peaks_magnitude_mV], 
-                    index = ['test_pulse_command_pA', 'injected_current_pA', 'holding_mV', 'peaks_magnitude_mV'],
-                    columns = trial_keys)
-
-                # Once we have computed all the parameters, we store the results in a dataframe
-                avg_AP_parameters_dataframe = pd.DataFrame([[
-                    np.round(np.mean(single_AP_parameters_df.loc['test_pulse_command_pA']), 2),
-                    np.round(np.mean(single_AP_parameters_df.loc['injected_current_pA']), 2),
-                    np.round(np.mean(single_AP_parameters_df.loc['holding_mV']), 2), 
-                    np.round(np.mean(single_AP_parameters_df.loc['peaks_magnitude_mV']), 2), 
-                    test_pulse_command_pA, injected_current_pA, holding_mV, peaks_magnitude_mV,
-                    cut_spikes, temp_avg_spike,
-                    temp_avg_spike_peak_index, temp_avg_spike_trough_index, temp_avg_spike_adp_index, temp_avg_spike_ahp_index,
-                    peak_mV, trough_mV, adp_peak_mV, ahp_trough_mV,
-                    peak_to_trough_magnitude, trough_to_adp_magnitude, adp_to_ahp_magnitude, 
-                    peak_to_trough_ms, trough_to_adp_ms, adp_to_ahp_ms,
-                    half_peak, temp_half_width_start, temp_half_width_end, half_width_ms,
-                    dvdt, dvdt_threshold_value, spike_threshold_mV, spike_threshold_index, threshold_to_peak_ms
-                    ]], 
-                    columns = ['command_pA', 'injected_pA', 'holding_mV', 'peaks_magnitude_mV',
-                                'command_bysweep_pA', 'injected_bysweep_pA', 'holding_bysweep_mV', 'peak_bysweep_mV', 
-                                'cut_spikes_traces_mV', 'avg_spike_trace_mV',
-                                'avg_spike_peak_index', 'avg_spike_trough_index', 'avg_spike_adp_index', 'avg_spike_ahp_index',
-                                'avg_spike_peak_mV', 'avg_spike_trough_mV', 'avg_spike_adp_mV', 'avg_spike_ahp_mV', 
-                                'peak_to_trough_mV', 'trough_to_adp_mV', 'adp_to_ahp_mV',
-                                'peak_to_trough_ms', 'trough_to_adp_ms', 'adp_to_ahp_ms',
-                                'half_peak_mV', 'half_width_start', 'half_width_end', 'half_width_ms',
-                                'dvdt_trace', 'dvdt_threshold_mV_ms', 'avg_spike_threshold_mV', 'avg_spike_threshold_index', 'threshold_to_peak_ms'
-                                ], 
-                    index = temp_file_id)
-            else:
-                print(f'Fix the recording from cell {temp_file_id} and try running getSpikeParameters() again')
-                plt.close()
-                return None # return empty variables to prevent wrong results from being used
-            
             plt.close()
+
+            # Create dataframe of results across sweeps:
+            single_AP_parameters_df = pd.DataFrame([
+                test_pulse_command_pA, injected_current_pA, holding_mV, peaks_magnitude_mV], 
+                index = ['test_pulse_command_pA', 'injected_current_pA', 'holding_mV', 'peaks_magnitude_mV'],
+                columns = trial_keys)
+
+            # Once we have computed all the parameters, we store the results in a dataframe
+            avg_AP_parameters_dataframe = pd.DataFrame([[
+                np.round(np.mean(single_AP_parameters_df.loc['test_pulse_command_pA']), 2),
+                np.round(np.mean(single_AP_parameters_df.loc['injected_current_pA']), 2),
+                np.round(np.mean(single_AP_parameters_df.loc['holding_mV']), 2), 
+                np.round(np.mean(single_AP_parameters_df.loc['peaks_magnitude_mV']), 2),
+                peak_mV, trough_mV, adp_peak_mV, ahp_trough_mV,
+                spike_threshold_mV, threshold_to_peak_ms, dvdt_threshold_value, 
+                half_peak_mV, half_width_ms,
+                peak_to_trough_magnitude, peak_to_trough_ms, 
+                trough_to_adp_magnitude, trough_to_adp_ms, 
+                adp_to_ahp_magnitude, adp_to_ahp_ms,
+                spike_threshold_index, temp_avg_spike_peak_index, temp_avg_spike_trough_index, temp_avg_spike_adp_index, temp_avg_spike_ahp_index, 
+                np.float64(temp_half_width_start), np.float64(temp_half_width_end),
+                test_pulse_command_pA, injected_current_pA, holding_mV, peaks_magnitude_mV,
+                cut_spikes, temp_avg_spike, dvdt
+                ]],
+                columns = ['command_pA', 'injected_pA', 'holding_mV', 'peaks_magnitude_mV',
+                            'avg_spike_peak_mV', 'avg_spike_trough_mV', 'avg_spike_adp_mV', 'avg_spike_ahp_mV',
+                            'avg_spike_threshold_mV', 'threshold_to_peak_ms', 'dvdt_threshold_mV_ms', 
+                            'half_peak_mV', 'half_width_ms',
+                            'peak_to_trough_mV', 'peak_to_trough_ms', 
+                            'trough_to_adp_mV', 'trough_to_adp_ms',
+                            'adp_to_ahp_mV', 'adp_to_ahp_ms', 
+                            'avg_spike_threshold_index', 'avg_spike_peak_index', 'avg_spike_trough_index', 'avg_spike_adp_index', 'avg_spike_ahp_index',
+                            'half_width_start', 'half_width_end',  
+                            'command_bysweep_pA', 'injected_bysweep_pA', 'holding_bysweep_mV', 'peak_bysweep_mV', 
+                            'cut_spikes_traces_mV', 'avg_spike_trace_mV', 'dvdt_trace'
+                            ],
+                index = temp_file_id)
+
             cell_temp_list.append(avg_AP_parameters_dataframe)
 
         folder_results_df = pd.concat(cell_temp_list) # concatenate all the data frames in the list
@@ -498,8 +476,7 @@ def getSpikeParameters(
     
     return folder_results_df # last pandas dataframe
 
-#%%n
-
+# %%
 # Choose folders
 vgat_dopamine_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vgat_dopamine"
 folders_to_check = [vgat_dopamine_save_path]
