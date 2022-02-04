0f8f72f41c6a620e0162281c81aed856d2177ece
opavon <oriol.pavon.16@ucl.ac.uk>
2022-02-04 19:34:24 +0000

    add summary of results

--- whole_cell/whole_cell_analysis_pipeline.py
+++ whole_cell/whole_cell_analysis_pipeline.py
@@ -76,6 +76,31 @@ last_folder_IR_results_df = getInputResistance(folders_to_check_IR, folder_to_sa
 # Inspect the output, corresponding to the dataframe from the last folder in the list
 last_folder_IR_results_df
 
+# %%
+# ## 1.2 | Quickly summarise input resistance results
+
+# Set path to results
+folder_to_save_IR = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_results\IC_tau_inputresistance"
+
+# Load .json files
+vgat_control_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vgat_control_pooled_input_resistance.json'))
+vgat_kyn_ptx_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vgat_kynurenic_picrotoxin_pooled_input_resistance.json'))
+vgat_ttx_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vgat_ttx_pooled_input_resistance.json'))
+vglut2_control_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vglut2_control_pooled_input_resistance.json'))
+vglut2_kyn_ptx_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vglut2_kynurenic_picrotoxin_pooled_input_resistance.json'))
+vglut2_ptx_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vglut2_picrotoxin_pooled_input_resistance.json'))
+vglut2_ttx_IR = pd.read_json(os.path.join(folder_to_save_IR, 'vglut2_ttx_pooled_input_resistance.json'))
+
+# Print average input resistance across conditions
+print('----- Input Resistance -----')
+print(f'The vgat_control input resistance is    {np.round(np.mean(vgat_control_IR[["IR_MOhm"]])[0], 2)} MOhm')
+print(f'The vgat_kyn_ptx input resistance is    {np.round(np.mean(vgat_kyn_ptx_IR[["IR_MOhm"]])[0], 2)} MOhm')
+print(f'The vgat_ttx input resistance is        {np.round(np.mean(vgat_ttx_IR[["IR_MOhm"]])[0], 2)} MOhm')
+print(f'The vglut2_control input resistance is  {np.round(np.mean(vglut2_control_IR[["IR_MOhm"]])[0], 2)} MOhm')
+print(f'The vglut2_kyn_ptx input resistance is  {np.round(np.mean(vglut2_kyn_ptx_IR[["IR_MOhm"]])[0], 2)} MOhm')
+print(f'The vglut2_ptx input resistance is      {np.round(np.mean(vglut2_ptx_IR[["IR_MOhm"]])[0], 2)} MOhm')
+print(f'The vglut2_ttx input resistance is      {np.round(np.mean(vglut2_ttx_IR[["IR_MOhm"]])[0], 2)} MOhm')
+
 # %% [markdown]
 # ## 2 | Get the action potential threshold and other parameters from all the curated cells in one or more folders
 
@@ -88,6 +113,7 @@ vgat_dopamine_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\ana
 vgat_kynac_ptx_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vgat_kynurenic_picrotoxin"
 vglut2_ctrl_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vglut2_control"
 vglut2_dopamine_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vglut2_dopamine"
+vglut2_kynac_ptx_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vglut2_kynurenic_picrotoxin"
 vglut2_ptx_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vglut2_picrotoxin"
 vglut2_ptx_leucine_single_AP_save_path = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_data\IC_single_AP\vglut2_picrotoxin_leucine"
 print("done!")
@@ -102,6 +128,7 @@ folders_to_check_single_AP = [vgat_agatoxin_single_AP_save_path,
                             vgat_kynac_ptx_single_AP_save_path,
                             vglut2_ctrl_single_AP_save_path,
                             vglut2_dopamine_single_AP_save_path,
+                            vglut2_kynac_ptx_single_AP_save_path,
                             vglut2_ptx_single_AP_save_path,
                             vglut2_ptx_leucine_single_AP_save_path]
 
@@ -120,3 +147,32 @@ last_folder_single_AP_results_df = getSpikeParameters(folders_to_check_single_AP
 
 # Inspect the output, corresponding to the dataframe from the last folder in the list
 last_folder_single_AP_results_df
+
+# %%
+# ## 2.2 | Quickly summarise single AP results
+
+# Set path to results
+folder_to_save_single_AP = r"D:\Dropbox (UCL)\Project_paginhibition\analysis\whole_cell\whole_cell_results\IC_single_AP"
+
+# Load .json files
+vgat_control = pd.read_json(os.path.join(folder_to_save_single_AP, 'vgat_control_pooled_single_AP_parameters.json'))
+vgat_kyn_ptx = pd.read_json(os.path.join(folder_to_save_single_AP, 'vgat_kynurenic_picrotoxin_pooled_single_AP_parameters.json'))
+vglut2_control = pd.read_json(os.path.join(folder_to_save_single_AP, 'vglut2_control_pooled_single_AP_parameters.json'))
+vglut2_kyn_ptx = pd.read_json(os.path.join(folder_to_save_single_AP, 'vglut2_kynurenic_picrotoxin_pooled_single_AP_parameters.json'))
+vglut2_ptx = pd.read_json(os.path.join(folder_to_save_single_AP, 'vglut2_picrotoxin_pooled_single_AP_parameters.json'))
+
+# Print average spike threshold and half-width across conditions
+print('----- Spike Threshold -----')
+print(f'The vgat_control spike threshold is     {np.round(np.mean(vgat_control[["avg_spike_threshold_mV"]])[0], 2)} mV')
+print(f'The vgat_kyn_ptx spike threshold is     {np.round(np.mean(vgat_kyn_ptx[["avg_spike_threshold_mV"]])[0], 2)} mV')
+print(f'The vglut2_control spike threshold is   {np.round(np.mean(vglut2_control[["avg_spike_threshold_mV"]])[0], 2)} mV')
+print(f'The vglut2_kyn_ptx spike threshold is   {np.round(np.mean(vglut2_kyn_ptx[["avg_spike_threshold_mV"]])[0], 2)} mV')
+print(f'The vglut2_ptx spike threshold is       {np.round(np.mean(vglut2_ptx[["avg_spike_threshold_mV"]])[0], 2)} mV')
+print('\n----- Spike Half-width -----')
+print(f'The vgat_control half-width is          {np.round(np.mean(vgat_control[["half_width_ms"]])[0], 2)} ms')
+print(f'The vgat_kyn_ptx half-width is          {np.round(np.mean(vgat_kyn_ptx[["half_width_ms"]])[0], 2)} ms')
+print(f'The vglut2_control half-width is        {np.round(np.mean(vglut2_control[["half_width_ms"]])[0], 2)} ms')
+print(f'The vglut2_kyn_ptx half-width is        {np.round(np.mean(vglut2_kyn_ptx[["half_width_ms"]])[0], 2)} ms')
+print(f'The vglut2_ptx half-width is            {np.round(np.mean(vglut2_ptx[["half_width_ms"]])[0], 2)} ms')
+
+# %%
