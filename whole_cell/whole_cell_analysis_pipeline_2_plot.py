8b7f60680b0ba54ba223fa059f7c99f6b88d38dc
opavon <oriol.pavon.16@ucl.ac.uk>
2022-05-17 12:20:59 +0100

    code to plot input resistance example traces

--- whole_cell/whole_cell_analysis_pipeline_2_plot.py
+++ whole_cell/whole_cell_analysis_pipeline_2_plot.py
@@ -61,42 +61,10 @@ print(f'The vglut2_ttx input resistance is          {np.round(np.mean(vglut2_ttx
 print(f'The vglut2_dopamine input resistance is     {np.round(np.mean(vglut2_dopamine_IR[["IR_MOhm"]])[0], 2)} +/- {np.round(np.std(vglut2_dopamine_IR[["IR_MOhm"]])[0], 2)} MOhm')
 
 # %% [markdown]
-# ### 1.2 | Plot sample traces for each conditions
+# ### 1.2 | Plot Input Resistance sample traces for each condition
 
 # %%
 # Once we have loaded the data, we can plot the average traces across conditions and export them for making figures.
-
-
-# %%
-# Load data
-channels_df, time, dt, folder_name, file_name = importFile(curated_channel = 'Sweeps_Analysis')
-print("file imported")
-
-InputR_df, InputR_avg_df = getCellInputResistance(file_name, channels_df)
-InputR_avg_df
-
-# %%
-## Potential vgat_ctrl sample traces
-# dmpag_vgat_170222_c3_OP_IC_tau_inputresistance_2_1 (no)
-# dmpag_vgat_201217_c3_WDIBM_OP_IC_tau_inputresistance_3_2 (no)
-# dmpag_vgat_201217_c5_WDIBO_OP_IC_tau_inputresistance_3_1 (yes)
-
-## Potential vgat_kynac_ptx sample traces
-# dmpag_vgat_180507_c6_WDIAF_OP_IC_tau_inputresistance_1_1_kyn_ptx (no)
-# dmpag_vgat_201218_c5_WDIBT_OP_IC_tau_inputresistance_3_2_kyn_ptx (yes)
-# dmpag_vgat_181210_c6_WDIAL_OP_IC_tau_inputresistance_2_1_kyn_ptx (no)
-
-## Potential vglut2_ctrl sample traces 
-# dlpag_vglut2_200722_c3_WEAL_OP_IC_tau_inputresistance_2_1 (yes)
-# dmpag_vglut2_200722_c7_WDEAT_OP_IC_tau_inputresistance_4_1
-# dmpag_vglut2_200711_c2_WEAF_OP_IC_tau_inputresistance_3_2
-
-
-## Potential vglut2_kynac_ptx sample traces 
-# dmpag_vglut2_180504_c2_WDEAF_OP_IC_tau_inputresistance_1_1_kyn_ptx (no)
-# dlpag_vglut2_180504_c4_WDEAH_OP_IC_tau_inputresistance_1_1_kyn_ptx (maybe)
-# dmpag_vglut2_180503_c2_WDEAB_OP_IC_tau_inputresistance_1_1_kyn_ptx (yes)
-
 
 # %%
 # Set colours
@@ -109,40 +77,20 @@ print('colours set!')
 # Set path to save folder
 folder_sample_traces_IR = r"D:\Dropbox (UCL - SWC)\Project_paginhibition\analysis\whole_cell\whole_cell_plots\IC_tau_inputresistance\sample_traces"
 curated_channel = 'Sweeps_Analysis'
-vgat_ctrl_sample_cell = 'dlpag_vglut2_180504_c4_WDEAH_OP_IC_tau_inputresistance_1_1_kyn_ptx.hdf5'
+print('path to results loaded!')
+
+# %%
+## Potential vgat_ctrl sample traces
+# dmpag_vgat_170222_c3_OP_IC_tau_inputresistance_2_1 (no)
+# dmpag_vgat_201217_c3_WDIBM_OP_IC_tau_inputresistance_3_2 (no)
+# dmpag_vgat_201217_c5_WDIBO_OP_IC_tau_inputresistance_3_1 (yes)
+
+# Select vgat_ctrl sample cell
+vgat_ctrl_sample_cell = 'dmpag_vgat_201217_c5_WDIBO_OP_IC_tau_inputresistance_3_1.hdf5'
 
 test_pulse_membrane_baselined, test_pulse_command_baselined, avg_test_pulse_membrane_baselined, avg_test_pulse_command_baselined, time, dt = getSampleTracesIR(folder_sample_traces_IR, vgat_ctrl_sample_cell, curated_channel)
 
-# channels_dataframe, time, dt = openFile(os.path.join(folder_sample_traces_IR, 'dlpag_vglut2_180504_c4_WDEAH_OP_IC_tau_inputresistance_1_1_kyn_ptx.hdf5'), curated_channel = 'Sweeps_Analysis') # extract channels from current file
-
-# test_pulse_command_baselined = []
-# test_pulse_membrane_baselined = []
-
-# # Calculate the input resistance on a sweep-by-sweep basis:
-# for sweep in channels_dataframe.columns:
-#     ## Load sweep data: Channel A (voltage recording in current-clamp), Channel B (injected current) and Output B (command)
-#     sweep_IA = np.array(channels_dataframe.at['Channel A', sweep]) # voltage
-#     sweep_OA = np.array(channels_dataframe.at['Output A', sweep]) # command
-
-#     ## Get the indices corresponding to the test_pulse using the Output Channel
-#     test_pulse = np.where(sweep_OA < 0)
-#     test_pulse_OA_indices = test_pulse[0]
-
-#     sweep_OA_baseline = np.mean(sweep_OA[:(test_pulse_OA_indices[0]-1)]) # -1 to stop baseline before command starts
-#     sweep_IA_baseline = np.mean(sweep_IA[:(test_pulse_OA_indices[0])])
-
-#     ## Baseline sweeps
-#     baselined_sweep_OA = sweep_OA - sweep_OA_baseline
-#     baselined_sweep_IA = sweep_IA - sweep_IA_baseline
-#     # Append results
-#     test_pulse_command_baselined.append(baselined_sweep_OA)
-#     test_pulse_membrane_baselined.append(baselined_sweep_IA)
-
-# # Compute average trace from all baselined sweeps
-# avg_test_pulse_command_baselined = np.array(np.mean(test_pulse_command_baselined, 0))
-# avg_test_pulse_membrane_baselined = np.array(np.mean(test_pulse_membrane_baselined, 0))
-
-# Plot
+# Plot protocol and cell's voltage response
 get_ipython().run_line_magic('matplotlib', 'qt')
 fig = plt.figure(tight_layout = True, figsize = (7, 10), dpi = 100) # Set figure size
 axs = fig.subplot_mosaic(
@@ -151,44 +99,153 @@ axs = fig.subplot_mosaic(
     BB
     """
 )
-# Plot protocol and cell's voltage response
+# Plot individual baselined sweeps
 for sweep in range(len(test_pulse_membrane_baselined)):
     axs['A'].plot(time[0], test_pulse_membrane_baselined[sweep], color = colour_vgat_ctrl)
+# Plot average sweep
 axs['A'].plot(time[0], avg_test_pulse_membrane_baselined, color = 'k')
-axs['A'].set_title('dlpag_vglut2_180504_c4_WDEAH', fontsize = 12)
-axs['A'].set_ylabel('voltage [mV]', fontsize = 10)
+axs['A'].set_title("Cell's membrane response", fontsize = 12)
+axs['A'].set_xlabel('time [ms]', fontsize = 12)
+axs['A'].set_ylabel('voltage [mV]', fontsize = 12)
 axs['A'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['A'].set_ylim([-20, 5])
+# Plot command
 axs['B'].plot(time[0][0:len(avg_test_pulse_command_baselined)], avg_test_pulse_command_baselined, color = 'k')
 axs['B'].set_title('Command', fontsize = 12)
-axs['B'].set_ylabel('current [pA]', fontsize = 10)
+axs['B'].set_xlabel('time [ms]', fontsize = 12)
+axs['B'].set_ylabel('current [pA]', fontsize = 12)
 axs['B'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
-
-
-# %%
-
-
-vgat_control_IR
-vgat_kyn_ptx_IR
-vglut2_control_IR
-vglut2_kyn_ptx_IR
-vgat_control_IR['membrane_avg_trace_mV']
-#vgat_ctrl_average_IR
-
-# %%
-# Plot vgat_ctrl example cell
-get_ipython().run_line_magic('matplotlib', 'qt')
-fig = plt.figure(figsize = (7, 5), dpi = 100) # Set figure size
-for s in range(len(vgat_control_IR['membrane_avg_trace_mV'])):
-    plt.plot(vgat_control_IR['membrane_avg_trace_mV'][s], colour_vgat_ctrl)
-# plt.plot(vgat_ctrl_average_IR, color = 'k')
-plt.title('VGAT control', fontsize = 14)
-plt.xlabel('samples', fontsize = 12)
-plt.ylabel('current [pA]', fontsize = 12)
-# plt.xlim(120, 195) # 25 samples = 1 ms
-# plt.ylim(-200, 125) # pA
+axs['B'].set_ylim([-25, 5])
+plt.suptitle('\nVGAT control: dmpag_vgat_201217_c5_WDIBO\n', fontsize = 14)
 fig.canvas.manager.window.move(0, 0)
 plt.show()
-# plt.savefig(os.path.join(folder_sample_traces_IR, 'vgat_ctrl_input_resistance.eps'), format = 'eps') # save figure as .eps
+plt.savefig(os.path.join(folder_sample_traces_IR, 'vgat_ctrl_input_resistance_WDIBO.eps'), format = 'eps') # save figure as .eps
+
+# %%
+## Potential vgat_kynac_ptx sample traces
+# dmpag_vgat_180507_c6_WDIAF_OP_IC_tau_inputresistance_1_1_kyn_ptx (no)
+# dmpag_vgat_201218_c5_WDIBT_OP_IC_tau_inputresistance_3_2_kyn_ptx (yes)
+# dmpag_vgat_181210_c6_WDIAL_OP_IC_tau_inputresistance_2_1_kyn_ptx (no)
+
+# Select vgat_ctrl sample cell
+vgat_kynac_ptx_sample_cell = 'dmpag_vgat_201218_c5_WDIBT_OP_IC_tau_inputresistance_3_2_kyn_ptx.hdf5'
+
+test_pulse_membrane_baselined, test_pulse_command_baselined, avg_test_pulse_membrane_baselined, avg_test_pulse_command_baselined, time, dt = getSampleTracesIR(folder_sample_traces_IR, vgat_kynac_ptx_sample_cell, curated_channel)
+
+# Plot protocol and cell's voltage response
+get_ipython().run_line_magic('matplotlib', 'qt')
+fig = plt.figure(tight_layout = True, figsize = (7, 10), dpi = 100) # Set figure size
+axs = fig.subplot_mosaic(
+    """
+    AA
+    BB
+    """
+)
+# Plot individual baselined sweeps
+for sweep in range(len(test_pulse_membrane_baselined)):
+    axs['A'].plot(time[0], test_pulse_membrane_baselined[sweep], color = colour_vgat_kynac_ptx)
+# Plot average sweep
+axs['A'].plot(time[0], avg_test_pulse_membrane_baselined, color = 'k')
+axs['A'].set_title("Cell's membrane response", fontsize = 12)
+axs['A'].set_xlabel('time [ms]', fontsize = 12)
+axs['A'].set_ylabel('voltage [mV]', fontsize = 12)
+axs['A'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['A'].set_ylim([-20, 5])
+# Plot command
+axs['B'].plot(time[0][0:len(avg_test_pulse_command_baselined)], avg_test_pulse_command_baselined, color = 'k')
+axs['B'].set_title('Command', fontsize = 12)
+axs['B'].set_xlabel('time [ms]', fontsize = 12)
+axs['B'].set_ylabel('current [pA]', fontsize = 12)
+axs['B'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['B'].set_ylim([-25, 5])
+plt.suptitle('\nVGAT in kynac_ptx: dmpag_vgat_201218_c5_WDIBT\n', fontsize = 14)
+fig.canvas.manager.window.move(0, 0)
+plt.show()
+plt.savefig(os.path.join(folder_sample_traces_IR, 'vgat_kynac_ptx_input_resistance_WDIBT.eps'), format = 'eps') # save figure as .eps
+
+# %%
+## Potential vglut2_ctrl sample traces 
+# dlpag_vglut2_200722_c3_WEAL_OP_IC_tau_inputresistance_2_1 (yes)
+# dmpag_vglut2_200722_c7_WDEAT_OP_IC_tau_inputresistance_4_1 (maybe)
+# dmpag_vglut2_200711_c2_WEAF_OP_IC_tau_inputresistance_3_2 (no)
+
+# Select vgat_ctrl sample cell
+vglut2_ctrl_sample_cell = 'dlpag_vglut2_200722_c3_WEAL_OP_IC_tau_inputresistance_2_1.hdf5'
+
+test_pulse_membrane_baselined, test_pulse_command_baselined, avg_test_pulse_membrane_baselined, avg_test_pulse_command_baselined, time, dt = getSampleTracesIR(folder_sample_traces_IR, vglut2_ctrl_sample_cell, curated_channel)
+
+# Plot protocol and cell's voltage response
+get_ipython().run_line_magic('matplotlib', 'qt')
+fig = plt.figure(tight_layout = True, figsize = (7, 10), dpi = 100) # Set figure size
+axs = fig.subplot_mosaic(
+    """
+    AA
+    BB
+    """
+)
+# Plot individual baselined sweeps
+for sweep in range(len(test_pulse_membrane_baselined)):
+    axs['A'].plot(time[0], test_pulse_membrane_baselined[sweep], color = colour_vglut2_ctrl)
+# Plot average sweep
+axs['A'].plot(time[0], avg_test_pulse_membrane_baselined, color = 'k')
+axs['A'].set_title("Cell's membrane response", fontsize = 12)
+axs['A'].set_xlabel('time [ms]', fontsize = 12)
+axs['A'].set_ylabel('voltage [mV]', fontsize = 12)
+axs['A'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['A'].set_ylim([-20, 5])
+# Plot command
+axs['B'].plot(time[0][0:len(avg_test_pulse_command_baselined)], avg_test_pulse_command_baselined, color = 'k')
+axs['B'].set_title('Command', fontsize = 12)
+axs['B'].set_xlabel('time [ms]', fontsize = 12)
+axs['B'].set_ylabel('current [pA]', fontsize = 12)
+axs['B'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['B'].set_ylim([-25, 5])
+plt.suptitle('\nVGluT2 control: dlpag_vglut2_200722_c3_WEAL\n', fontsize = 14)
+fig.canvas.manager.window.move(0, 0)
+plt.show()
+plt.savefig(os.path.join(folder_sample_traces_IR, 'vglut2_ctrl_input_resistance_WEAL.eps'), format = 'eps') # save figure as .eps
+
+# %%
+## Potential vglut2_kynac_ptx sample traces 
+# dmpag_vglut2_180504_c2_WDEAF_OP_IC_tau_inputresistance_1_1_kyn_ptx (no)
+# dlpag_vglut2_180504_c4_WDEAH_OP_IC_tau_inputresistance_1_1_kyn_ptx (no)
+# dmpag_vglut2_180503_c2_WDEAB_OP_IC_tau_inputresistance_1_1_kyn_ptx (yes)
+
+# Select vglut2_kynac_ptx sample cell
+vglut2_kynac_ptx_sample_cell = 'dmpag_vglut2_180503_c2_WDEAB_OP_IC_tau_inputresistance_1_1_kyn_ptx.hdf5'
+
+test_pulse_membrane_baselined, test_pulse_command_baselined, avg_test_pulse_membrane_baselined, avg_test_pulse_command_baselined, time, dt = getSampleTracesIR(folder_sample_traces_IR, vglut2_kynac_ptx_sample_cell, curated_channel)
+
+# Plot protocol and cell's voltage response
+get_ipython().run_line_magic('matplotlib', 'qt')
+fig = plt.figure(tight_layout = True, figsize = (7, 10), dpi = 100) # Set figure size
+axs = fig.subplot_mosaic(
+    """
+    AA
+    BB
+    """
+)
+# Plot individual baselined sweeps
+for sweep in range(len(test_pulse_membrane_baselined)):
+    axs['A'].plot(time[0], test_pulse_membrane_baselined[sweep], color = colour_vglut2_kynac_ptx)
+# Plot average sweep
+axs['A'].plot(time[0], avg_test_pulse_membrane_baselined, color = 'k')
+axs['A'].set_title("Cell's membrane response", fontsize = 12)
+axs['A'].set_xlabel('time [ms]', fontsize = 12)
+axs['A'].set_ylabel('voltage [mV]', fontsize = 12)
+axs['A'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['A'].set_ylim([-20, 5])
+# Plot command
+axs['B'].plot(time[0][0:len(avg_test_pulse_command_baselined)], avg_test_pulse_command_baselined, color = 'k')
+axs['B'].set_title('Command', fontsize = 12)
+axs['B'].set_xlabel('time [ms]', fontsize = 12)
+axs['B'].set_ylabel('current [pA]', fontsize = 12)
+axs['B'].set_xlim([0, (len(avg_test_pulse_membrane_baselined)*dt)])
+axs['B'].set_ylim([-25, 5])
+plt.suptitle('\nVGluT2 in kynac_ptx: dmpag_vglut2_180503_c2_WDEAB\n', fontsize = 14)
+fig.canvas.manager.window.move(0, 0)
+plt.show()
+plt.savefig(os.path.join(folder_sample_traces_IR, 'vglut2_kynac_ptx_input_resistance_WDEAB.eps'), format = 'eps') # save figure as .eps
 
 # %% [markdown]
 # ## 2.0 | Single Action Potential characterisation
